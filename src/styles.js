bc9d40519e7204ccf685f3effe63016b8abc4500
Gui12x9 <guilhermejesus568@gmail.com>
2024-06-12 14:44:03 -0300

    project react

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import Backgroud from './assets/backgroud1.svg'
+
+export const Container = styled.div`
+    background: url("${Backgroud}");
+    background-size: cover;
+`
+
+export const Image = styled.img` `
+
+export const ContainerItens = styled.div` `
+
+export const H1 = styled.h1` `
+
+export const InputLabel = styled.p` `
+
+export const Input = styled.input` `
+
+export const Button = styled.button` `
