113f24644d1147c5f6accb5afeb96b5e6dca7464
Gui12x9 <guilhermejesus568@gmail.com>
2024-06-18 22:44:47 -0300

    commit-2

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,10 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle `
+export default createGlobalStyle`
 
 *{
     padding:0;
     margin:0;
-    box-sizing:border-box
+    box-sizing:border-box;
+  
+
+  font-family: "Roboto Condensed", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+
 }
-`
+
+
+`;
--- src/styles.js
+++ src/styles.js
@@ -4,22 +4,125 @@ import Backgroud from "./assets/backgroud.svg";
 export const Container = styled.div`
   background: url("${Backgroud}");
   display: flex;
+  gap: 35px;
+  height: 100vh;
   flex-direction: column;
   align-items: center;
-  gap: 40px;
   background-size: cover;
 `;
 
 export const Image = styled.img`
-margin-top:30px;
+  margin-top: 30px;
 `;
 
-export const ContainerItens = styled.div``;
+export const ContainerItens = styled.div`
+  background: linear-gradient(
+    157.44deg,
+    rgba(255, 255, 255, 0.6) 0.84%,
+    rgba(255, 255, 255, 0.6) 0.85%,
+    rgba(255, 255, 255, 0.15) 100%
+  );
+  height: 100vh;
+  border-radius: 61px 61px 0px 0px;
+  padding: 50px 36px;
+  display: flex;
+  flex-direction: column;
+`;
 
-export const H1 = styled.h1``;
+export const H1 = styled.h1`
+  font-style: normal;
+  font-weight: bold;
+  line-height: 40px;
+  color: #ffffff;
+  text-align: center;
+  margin-bottom: 80px;
+`;
 
-export const InputLabel = styled.p``;
+export const InputLabel = styled.p`
+  letter-spacing: -0.408px;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 22px;
+  font-weight: bold;
+  color: #eeeeee;
+  margin-left: 25px;
+`;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
 
-export const Button = styled.button``;
+  width: 342px;
+  height: 58px;
+  padding-left: 25px;
+  margin-bottom: 34px;
+
+  outline: none;
+  border: none;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 28px;
+  color: #ffffff;
+`;
+
+export const Button = styled.button`
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 14px;
+  border: none;
+  width: 342px;
+  height: 74px;
+
+  font-style: normal;
+  font-weight: bold;
+  color: #ffffff;
+  font-size: 17px;
+  line-height: 28px;
+
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  align-items: center;
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.5;
+  }
+`;
+
+export const User = styled.li`
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+
+  outline: none;
+  border: none;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 20px;
+
+  width: 342px;
+  height: 58px;
+  padding-left: 25px;
+
+  p {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 19px;
+    line-height: 28px;
+
+    color: #ffffff;
+  }
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+`;
